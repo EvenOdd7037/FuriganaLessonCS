0577f7f85fea491482a23377f194b5d70af945fb
EvenOdd7037 <hirocky1998@icloud.com>
2026-04-23 16:06:29 +0900

    Chapter1-No09まで進めました

---
+++ Assets/Chap1_8_1.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Chap1_8_1 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        string text = "ここはテキトーに何か書いていいらしい";
+        Debug.Log(text);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Chap1_8_2.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class Chap1_8_2 : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        double kakaku = 150;
+        double urine = kakaku * 1.27;
+        Debug.Log(urine);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
